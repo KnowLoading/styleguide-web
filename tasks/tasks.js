7733da77ed18bbbd696ab76de5399e674ae7cbf1
Alb Pérez-Bermejo <albperezbermejo@gmail.com>
2015-10-06 12:11:57 +0200

    chore: rename tasks to serve && serve:dist

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -4,7 +4,7 @@ module.exports = ($) => {
     $.gulp.task('compiledMin', (cb) => $.runSequence('scripts', ['styles-dist', 'jade-dist', 'copy'], 'copyDeploy', 'templateCache-dist', 'distTask', cb))
 
     $.gulp.task('analize', (cb) => $.runSequence('compiledBase', 'analysis', cb))
-    $.gulp.task('dist', (cb) => $.runSequence('clean', 'compiledMin', 'webserver-dist', cb))
-    $.gulp.task('run', (cb) => $.runSequence('clean', 'compiledBase', 'webserver', cb))
+    $.gulp.task('serve:dist', (cb) => $.runSequence('clean', 'compiledMin', 'webserver-dist', cb))
+    $.gulp.task('serve', (cb) => $.runSequence('clean', 'compiledBase', 'webserver', cb))
     $.gulp.task('test', (cb) => $.runSequence('clean', 'compiledBase', 'scripts-js-test', 'karma', cb))
 }
